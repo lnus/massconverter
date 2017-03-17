3597b95249baf1bd57189f7d072c4d7d861223dc
Linus <linuzlp@gmail.com>
2017-03-17 21:00:10 +0100

    :circus_tent: Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import time
+import glob
+
+class Converter(object):
+    """Converts all the files in the current location to mp3"""
+    def __init__(self):
+        self.path = os.getcwd()
+        if not os.path.exists("Finished"):
+            os.system("mkdir Finished")
+        self.formats = ["ogg", "mp4", "wav"]
+        self.files = []
+
+    def find_files(self):
+        """finds all the files in self.path"""
+        for f in self.formats:
+            for item in glob.glob("*.{}".format(f)):
+                self.files.append(item)
+    
+    def convert_files(self):
+        """converts all files in self.files"""
+        for f in self.files:
+            filename = f[:-4] 
+            os.system("ffmpeg -i {} Finished\{}".format(f, filename+".mp3"))
+        
+def main():
+    """Main function"""
+    c = Converter() 
+    c.find_files()
+    c.convert_files()
+
+if __name__ == '__main__':
+    main()
